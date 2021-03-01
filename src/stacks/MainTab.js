43ef4ea6075f353654fd681bae7b01693db4b2f7
Waldener Monteiro <jjrr019@gmail.com>
2021-03-01 00:29:06 -0300

    refactor: define tabBar custom default

--- src/stacks/MainTab.js
+++ src/stacks/MainTab.js
@@ -6,15 +6,11 @@ import Search from '../views/Search';
 import Appointments from '../views/Appointments';
 import Favorites from '../views/Favorites';
 import Profile from '../views/Profile';
-
+import TabBar from '../components/tabBar';
 const Tab = createBottomTabNavigator();
 export default () => {
   return (
-    <Tab.Navigator
-      initialRouteName="Home"
-      screenOptions={{
-        headerShown: false,
-      }}>
+    <Tab.Navigator tabBar={(props) => <TabBar {...props} />}>
       <Tab.Screen name="Home" component={Home} />
       <Tab.Screen name="Search" component={Search} />
       <Tab.Screen name="Appointments" component={Appointments} />
