d177646ce3fa32a365be9f6cdca9a34b33e43447
Waldener Monteiro <jjrr019@gmail.com>
2021-03-07 18:48:48 -0300

    refactor: style home

--- src/views/Home/styles.js
+++ src/views/Home/styles.js
@@ -4,6 +4,47 @@ import styled from 'styled-components/native';
 export const Container = styled.SafeAreaView`
   background-color: #63c2d1;
   flex: 1;
-  justify-content: center;
+  /* justify-content: center;
+  align-items: center; */
+`;
+export const HeaderArea = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
   align-items: center;
+`;
+export const HeaderTitle = styled.Text`
+  width: 250px;
+  font-size: 24px;
+  font-weight: bold;
+  color: #fff;
+`;
+export const LocationArea = styled.View`
+  background-color: #4eadbe;
+  height: 60px;
+  border-radius: 30px;
+  flex-direction: row;
+  align-items: center;
+  padding-left: 20px;
+  padding-right: 20px;
+  margin-top: 30px;
+`;
+export const LocationInput = styled.TextInput`
+  flex: 1;
+  font-size: 16px;
+  color: #ffffff;
+`;
+export const LocationFinder = styled.TouchableOpacity`
+  width: 24px;
+  height: 24px;
+`;
+export const LoadingIcon = styled.ActivityIndicator`
+  margin-top: 50px;
+`;
+export const SearchButton = styled.TouchableOpacity`
+  width: 26px;
+  height: 26px;
+`;
+export const Scroller = styled.ScrollView`
+  flex: 1;
+  padding: 20px;
 `;
